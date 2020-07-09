f95a9867fac502f54125655cff253c66b64df717
Your Name <you@example.com>
2020-07-09 19:30:12 +0900

    My Django Girls app, first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\riku1\documents\research\study\django\djangogirls\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
